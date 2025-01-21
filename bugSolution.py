03f559367d131a00458dd00c56c76021a9936db0
BugHunter-X <askzenai@gmail.com>
2025-01-21 12:44:06 +0100

    Add bugSolution.py file

---
+++ bugSolution.py
@@ -0,0 +1,16 @@
+def function_with_uncommon_error_solution(a, b):
+    if a == 0 and b == 0:
+        raise ZeroDivisionError("Both inputs cannot be zero.")
+    elif a == 0:
+        return b
+    elif b == 0:
+        return a
+    else:
+        return a / b
+
+try:
+    result = function_with_uncommon_error_solution(0, 0)
+    print(result)
+except ZeroDivisionError as e:
+    print(e) 
+    
